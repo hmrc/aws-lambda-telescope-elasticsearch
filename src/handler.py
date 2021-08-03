028a367a0e2911e77b49746a1b8de0fe828e6f21
Stephen Palfreyman <s.palfreyman@gmail.com>
2021-08-03 20:04:18 +0100

    TEL-2442 add requests to dependencies

--- src/handler.py
+++ src/handler.py
@@ -1,9 +1,9 @@
-from src.send_graphyte_message import publish_to_graphite
-from src.index_tools import (
+from send_graphyte_message import publish_to_graphite
+from index_tools import (
     get_indices_size_in_bytes,
     get_number_writable_indices_shards,
 )
-from src.elasticsearch_api import get_indices, get_indices_stats
+from elasticsearch_api import get_indices, get_indices_stats
 from aws_lambda_powertools import Logger
 import os
 
--- src/send_graphyte_message.py
+++ src/send_graphyte_message.py
@@ -1,6 +1,6 @@
 import graphyte
 
-from src.app_info import METRICS_PREFIX
+from app_info import METRICS_PREFIX
 
 
 def publish_to_graphite(path, metrics, graphite_host):
